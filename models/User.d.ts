6995f32881784f92cdf43f32102d7d52d93a91ae
James Lee <machingclee@gmail.com>
2020-04-10 16:35:42 +0800

    slight change

--- models/User.d.ts
+++
@@ -1,7 +0,0 @@
-import mongoose, { Document } from "mongoose";
-export interface IUser extends Document {
-    email: string;
-    password: string;
-}
-declare const _default: mongoose.Model<IUser, {}>;
-export default _default;
